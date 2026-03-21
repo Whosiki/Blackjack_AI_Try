cbc4f60dc513b2a53f42b93e156778b6986fdb59
airis <airisblackblood@gmail.com>
2026-03-21 16:19:37 +0400

    Simple 2 player blackjack

--- main.py
+++ main.py
@@ -14,7 +14,41 @@ for num in range(decnum):
     multideck.extend(fulldeck)
 random.shuffle(multideck)
 input1="0"
+input2="0"
+maxvalue=21
+p1score=0
+p2score=0
 while input1!="1":
     currentcard=multideck.pop(0)
-    print(currentcard,end="")
-    input1=input()
+    print(f"Your card is: {currentcard[0]} of {currentcard[1]}. ")
+    if currentcard[2]!=currentcard[3]:
+        choice=input(f"What value do you choose? [{currentcard[2]}] or [{currentcard[3]}]")
+        p1score+=int(choice)
+    else:
+        p1score+=currentcard[2]
+    print(f"Score: {p1score}")
+    if p1score>21:
+        break
+    if input("")=="Enough":
+        break
+while input2!="1":
+    currentcard=multideck.pop(0)
+    print(f"Your card is: {currentcard[0]} of {currentcard[1]}. ")
+    if currentcard[2]!=currentcard[3]:
+        choice=input(f"What value do you choose? [{currentcard[2]}] or [{currentcard[3]}]")
+        p2score+=int(choice)
+    else:
+        p2score+=currentcard[2]
+    print(f"Score: {p2score}")
+    if p2score>21:
+        break
+    if input("")=="Enough":
+        break
+if p2score>21:
+    print("Player 1 wins")
+elif p1score>21:
+    print("Player 2 wins")
+elif p1score>p2score:
+    print("Player 1 wins")
+else:
+    print("Player 2 wins")
