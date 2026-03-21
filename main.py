927f6e618ee8b24b6937fa9abbeae30445edbd6e
airis <airisblackblood@gmail.com>
2026-03-21 15:25:19 +0400

    Makes and shuffles decks

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import random
-A1=["Ace","Hearts",1,11]
-A2=["Ace","Diamonds",1,11]
-A3=["Ace","Clubs",1,11]
-A4=["Ace","Spades",1,11]
-Two1=["Two","Hearts",2,2]
-Two2=["Two","Diamonds",2,2]
-Two3=["Two","Clubs",2,2]
-Two4=["Two","Spades",2,2]
 fulldeck=[]
+multideck=[]
+deck="Deck"
+with open(deck,'r',encoding='utf-8') as file:
+    for line in file:
+        content= line.strip()
+        tempcardinfo=content.split(",")
+        tempcardinfo[2]=int(tempcardinfo[2])
+        tempcardinfo[3] = int(tempcardinfo[3])
+        fulldeck.append(tempcardinfo)
+decnum=int(input("How many decks for a game?: "))
+for num in range(decnum):
+    multideck.extend(fulldeck)
+random.shuffle(multideck)
+input1="0"
+while input1!="1":
+    currentcard=multideck.pop(0)
+    print(currentcard,end="")
+    input1=input()
